334ad7b13a2aae5a4de65d432fcef0f3849294c4
Kavin Phan <phan.kavin@gmail.com>
2020-03-21 03:32:51 -0400

    Consolidate Enderlift interactions to the class itself

--- src/main/java/com/kqp/enderlift/BlockEnderlift.java
+++ src/main/java/com/kqp/enderlift/BlockEnderlift.java
@@ -9,7 +9,11 @@ import net.minecraft.block.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 public class BlockEnderlift extends Block {
@@ -19,7 +23,29 @@ public class BlockEnderlift extends Block {
         super(FabricBlockSettings.of(Material.STONE).strength(15.0F, 1200.0F).build());
     }
 
-    public static BlockPos findOthers(World world, BlockPos pos, int range) {
+    public static void playNoise(World world, PlayerEntity player) {
+        world.playSound((PlayerEntity) null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.BLOCKS, 0.5F, 0.4F / (RANDOM.nextFloat() * 0.4F + 0.8F));
+    }
+
+    public ActionResult playerInteract(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult) {
+        BlockPos other = findOthers(world, hitResult.getBlockPos(), Enderlift.config.range * (player.isSneaking() ? -1 : 1));
+
+        if (hitResult.getSide() == Direction.UP && other != null) {
+            if (player.getX() > other.getX() && player.getZ() > other.getZ() && player.getX() < other.getX() + 1 && player.getZ() < other.getZ() + 1) {
+                BlockEnderlift.playNoise(world, player);
+
+                if (!world.isClient) {
+                    player.requestTeleport(player.getX(), other.getY() + 1, player.getZ());
+                }
+
+                return ActionResult.SUCCESS;
+            }
+        }
+
+        return ActionResult.PASS;
+    }
+
+    private static BlockPos findOthers(World world, BlockPos pos, int range) {
         BlockPos foundPos = null;
 
         int start = pos.getY() + (range > 0 ? 1 : -1);
@@ -36,9 +62,5 @@ public class BlockEnderlift extends Block {
         }
 
         return foundPos;
-    }
-
-    public static void playNoise(World world, PlayerEntity player) {
-        world.playSound((PlayerEntity) null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.BLOCKS, 0.5F, 0.4F / (RANDOM.nextFloat() * 0.4F + 0.8F));
     }
 }
--- src/main/java/com/kqp/enderlift/Enderlift.java
+++ src/main/java/com/kqp/enderlift/Enderlift.java
@@ -2,18 +2,14 @@ package com.kqp.enderlift;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
 
 public class Enderlift implements ModInitializer {
-	public static final Block ENDERLIFT = new BlockEnderlift();
+	public static final BlockEnderlift ENDERLIFT = new BlockEnderlift();
 
 	public static EnderliftConfig config;
 
@@ -26,22 +22,6 @@ public class Enderlift implements ModInitializer {
 		Registry.register(Registry.BLOCK, new Identifier("enderlift", "enderlift"), ENDERLIFT);
 		Registry.register(Registry.ITEM, new Identifier("enderlift", "enderlift"), new BlockItem(ENDERLIFT, new Item.Settings().group(ItemGroup.TRANSPORTATION)));
 
-		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-			BlockPos other = BlockEnderlift.findOthers(world, hitResult.getBlockPos(), config.range * (player.isSneaking() ? -1 : 1));
-
-			if (hitResult.getSide() == Direction.UP && other != null) {
-				if (player.getX() > other.getX() && player.getZ() > other.getZ() && player.getX() < other.getX() + 1 && player.getZ() < other.getZ() + 1) {
-					BlockEnderlift.playNoise(world, player);
-
-					if (!world.isClient) {
-						player.requestTeleport(player.getX(), other.getY() + 1, player.getZ());
-					}
-	
-					return ActionResult.SUCCESS;
-				}
-			}
-
-			return ActionResult.PASS;
-		});
+		UseBlockCallback.EVENT.register(ENDERLIFT::playerInteract);
 	}
 }
